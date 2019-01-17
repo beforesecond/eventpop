9eba464bdedcfa85da3ff3b8af2732e080b5e3eb
first <firstnaruto@msn.com>
2019-01-17 14:49:36 +0700

    [✨] Add range time fot trigger ticket.

--- cron.js
+++ cron.js
@@ -1,16 +1,23 @@
 var cron = require('node-cron')
+var moment = require('moment')
+
+var time1 = moment().unix()
+var time2 = moment('2019-01-17 17:55:00').unix()
+var time3 = moment('2019-01-17 18:05:00').unix()
 
 cron.schedule('* * * * * *', function() {
-  console.log('running a task every second')
+  if (time1 >= time2 && time1 <= time3) {
+    console.log('running a task every second')
 
-  const { exec } = require('child_process')
-  exec('npm run start', (err, stdout, stderr) => {
-    if (err) {
-      console.log(err)
-      // node couldn't execute the command
-      return
-    }
-    console.log(`stdout: ${stdout}`)
-    console.log(`stderr: ${stderr}`)
-  })
+    const { exec } = require('child_process')
+    exec('npm run start', (err, stdout, stderr) => {
+      if (err) {
+        console.log(err)
+        // node couldn't execute the command
+        return
+      }
+      console.log(`stdout: ${stdout}`)
+      console.log(`stderr: ${stderr}`)
+    })
+  }
 })
